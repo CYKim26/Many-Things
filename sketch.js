e3bfc4c3fac88efebbae40ded6a402cefca915da
CYKim26 <caden.kim26@avenues.org>
2024-12-08 18:29:44 -0500

    Update sketch.js
    Fixed ball collision with “ellipseMode(RADIUS)” in setup; refactored/reorganized the for loops in draw; added user rectangle object for user interaction; added (imperfect) ball-rectangle collisions with a couple new conditionals

--- sketch.js
+++ sketch.js
@@ -1,19 +1,27 @@
 let balls
 
 function setup() {
+  ellipseMode(RADIUS)
   createCanvas(windowWidth, windowHeight);
-    noStroke()
+  noStroke()
+  
   createBalls(int(random(10,15)))
-
+  u = {
+    x: mouseX,
+    y: mouseY,
+    w: 50,
+    h: 50,
+    c: color(255)
+  }
 }
 
 function createBalls(n) {
 //   n: number of balls
-
   balls = []
   
   for( let i = 0; i < n; i++ ) {
-    r=random(15,30)
+    r = random(7,14)
+  
     let newBall = {
       r: r,
       x: random(r, width-r),
@@ -25,11 +33,43 @@ function createBalls(n) {
     balls.push(newBall)
   }
 }
+
+
 function draw() {
   background(0)
   
+  fill(u.c)
+  rect(u.x,u.y,u.w,u.h)
+  u.x = mouseX - u.w/2
+  u.y = mouseY - u.h/2
+
   for(let i=0;i<balls.length;i++) {
     b = balls[i]
+    fill(b.c)
+    circle(b.x,b.y,b.r)
+    b.x += b.dx 
+    b.y += b.dy
+    if( b.x < b.r || b.x > width - b.r) {
+      b.dx *= -1
+      b.c.setAlpha(random(50,255))
+    }
+    if( b.y < b.r || b.y > height - b.r) {
+      b.dy *= -1
+      b.c.setAlpha(random(50,255))
+    }
+    
+    if (u.x < b.x + b.r && u.x + u.w > b.x - b.r && u.y < b.y + b.r && u.y + u.h > b.y - b.r) {
+      let overlapX = abs((u.x + u.w / 2) - b.x)
+      let overlapY = abs((u.y + u.h / 2) - b.y)
+      
+      if (overlapX > overlapY) {
+        b.dx *= -1
+      } else {
+        b.dy *= -1
+      }
+    }
+  
+    
     for (let j=i+1;j<balls.length;j++) {
       b2 = balls[j]
       if(dist(b.x,b.y,b2.x,b2.y) < b.r + b2.r) {
@@ -37,19 +77,4 @@ function draw() {
       }
     }
   }
-  for (let b of balls) {
-    fill(b.c)
-    circle(b.x,b.y,b.r)
-    b.x += b.dx 
-    b.y += b.dy
-    if( b.x < b.r || b.x > width- b.r) {
-      b.dx *= -1
-      b.c.setAlpha(random(50,255))
-    }
-    if( b.y < b.r || b.y > height - b.r ) {
-      b.dy *= -1
-      b.c.setAlpha(random(50,255))
-    }
-  }
-  
 }
